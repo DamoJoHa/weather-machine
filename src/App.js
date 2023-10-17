d9f3ff6b978f7044108d57426938dc9b5a01eced
Damian Hallengren <djhallengren@gmail.com>
2023-10-17 20:35:15 +0200

    implemented logic for api calls given coordinates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const NEWYORK = [40.7128, -74.0060]
+  const [temp, setTemp] = useState(0)
+  const [humid, setHumid] = useState(0)
+  const [precip, setPrecip] = useState(0)
+
+  // Get location (will rely on search box stuff)
+  function mapRequest() {
+
+  }
+
+
+  // Call Weather API (this can be consolidated)
+  function weatherCall({coords}) {
+    // We'll need this later because the national weather service has a two request system
+    // const url = `https://api.weather.gov/points/${coords[0]},${coords[1]}`
+
+    const url = `https://api.weather.gov/gridpoints/OKX/33,35/forecast`
+    const stats = call(url)
+
+    console.log(stats)
+  }
+
+  function call(url) {
+    let stats = {temp: null, tempTrend: null, humidity: null, precip: null}
+    fetch(url, {method: "GET"})
+      .then((response) => response.json())
+      .then((response) => {
+        const today = response.properties.periods[0]
+        setTemp(today.temperature)
+        // setTempTrend(today.temperatureTrend)
+        setHumid(today.relativeHumidity.value)
+        setPrecip(today.probabilityOfPrecipitation.value)
+    });
+    console.log(stats.temp)
+    return stats
+  }
+
+  weatherCall(NEWYORK)
+
+  // Display info (with various intermediate steps tbd)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{temp}F</p>
+      <p>Humidity: {humid}%</p>
+      <p>Chance of Rain: {precip}%</p>
     </div>
   );
 }
-
-export default App;
