1ee6977853b09f552aea4aeb9b85932b289b7891
Damian Hallengren <djhallengren@gmail.com>
2023-10-21 21:30:53 -0400

    very basic working app
    '

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ const FLIGHTCODE = "NK712"
 export default function App() {
   //Use Date
   const [departure, setDeparture] = useState({time: null, name: null, lat: null, lon: null})
-  const [arrival, setArrival] = useState({time: null, name: null, coords: null})
-
+  const [arrival, setArrival] = useState({time: null, name: null, lat: null, lon: null})
 
   //Takes user input code and sets state for flight info
-  function flightCall(code) {
+  function handleSumbit(e) {
+    e.preventDefault();
+
+    //handle form data
+    const formData = new FormData(e.target)
+    const formJson = Object.fromEntries(formData.entries());
+    const code = formJson.code
+
+
+    //API call to get flight data
     const url = `https://flight-radar1.p.rapidapi.com/flights/get-more-info?query=${code}&fetchBy=flight&page=1`
     fetch(url, {
       method: "GET",
@@ -23,41 +31,54 @@ export default function App() {
     }})
       .then((response) => response.json())
       .then((response) => {
-        console.log(response)
-        const data = response.response.data[0]
+        // console.log(response)
+        const data = response.result.response.data[0]
+
         //departure vars
         let name = data.airport.origin.name
         let lat = data.airport.origin.position.latitude
         let lon = data.airport.origin.position.longitude
         let time = data.time.scheduled.departure
         setDeparture({time: time, name: name, lat: lat, lon: lon})
-        console.log(departure)
+
         //arrival vars
         name = data.airport.destination.name
         lat = data.airport.destination.position.latitude
         lon = data.airport.destination.position.longitude
         time = data.time.scheduled.arrival
         setArrival({time: time, name: name, lat: lat, lon: lon})
-        console.log(arrival)
-      });
+      })
+  }
+
+  function SearchBar({onSubmit}) {
+    return (
+      <form className="search-bar" onSubmit={onSubmit}>
+          <input name="code" placeholder="Flight Number"></input>
+          <button type="submit">Search</button>
+      </form>
+    )
   }
 
   return (
     <div className="container">
-      <input id="flight-search" placeholder="Flight Number"></input>
-      <WeatherCard city={NEWYORK}/>
+      <SearchBar onSubmit={handleSumbit}/>
+      <div className="cards">
+        <WeatherCard city={arrival}/>
+        <WeatherCard city={departure}/>
+      </div>
     </div>
   )
 }
+
+
 
 function WeatherCard({city}) {
 
   const [temp, setTemp] = useState(null)
   const [humid, setHumid] = useState(null)
-  const [precip, setPrecip] = useState(null)
   const [icon, setIcon] = useState(null)
   const [wind, setWind] = useState(null)
-  const [tempTrend, setTempTrend] = useState(null)
+  const [desc, setDesc] = useState(null)
 
   // Call Weather API (this can be consolidated)
   function weatherCall(lat, lon) {
@@ -75,18 +96,18 @@ function WeatherCard({city}) {
     fetch(url, {method: "GET"})
       .then((response) => response.json())
       .then((response) => {
+        // need logic to decide which period to use
         const today = response.properties.periods[0]
-        // console.log(today)
+        console.log(today)
         setTemp(today.temperature)
-        setTempTrend(today.temperatureTrend)
+        setDesc(today.shortForecast)
         setHumid(today.relativeHumidity.value)
-        setPrecip(today.probabilityOfPrecipitation.value)
         setIcon(today.icon)
         setWind(today.windSpeed)
     });
   }
 
-  weatherCall(city.lat, city.lon)
+  if (city.name) weatherCall(city.lat, city.lon);
 
   // Display info (with various intermediate steps tbd)
   return (
@@ -97,10 +118,9 @@ function WeatherCard({city}) {
       <div className="content-row">
         <img src={icon} alt="weather icon" className="weather-icon"/>
         <h3>{temp}°F</h3>
-        <p>Temperature: {tempTrend}</p>
+        <p>{desc}</p>
         <p>Wind: {wind}</p>
         <p>Humidity: {humid}%</p>
-        <p>Chance of Rain: {precip ? `${precip}%` : "Unknown" }</p>
       </div>
     </div>
   );
