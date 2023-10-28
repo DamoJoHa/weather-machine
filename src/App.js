b72a3ef7980efff0ecc7f1ea397e743d406c9165
Damian Hallengren <djhallengren@gmail.com>
2023-10-28 00:58:06 -0400

    I'm tired but I think this all makes sense

--- src/App.js
+++ src/App.js
@@ -1,53 +1,74 @@
 import './App.css';
 import { useState } from 'react';
 
-//Vars for testing
-const NEWYORK = {time: null, name: "New York", lat: 40.7128, lon: -74.0060}
-const FLIGHTCODE = "NK712"
 
-//Do two of these for flight departure and arrival location and times :)
 export default function App() {
-  //Use Date
+  // need new states to keep track of
   const [departure, setDeparture] = useState({time: null, name: null, lat: null, lon: null})
   const [arrival, setArrival] = useState({time: null, name: null, lat: null, lon: null})
 
   //Takes user input code and sets state for flight info
   function handleSumbit(e) {
     e.preventDefault();
+    console.log("Code received")
 
-    //handle form data
+    // REGEX the code here?
+
+    // Handle form data
     const formData = new FormData(e.target)
     const formJson = Object.fromEntries(formData.entries());
     const code = formJson.code
 
 
-    //API call to get flight data
+    // API call to get flight data
     const url = `https://flight-radar1.p.rapidapi.com/flights/get-more-info?query=${code}&fetchBy=flight&page=1`
+    console.log("Fetching flight info")
     fetch(url, {
       method: "GET",
       headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_APIKEY,
       'X-RapidAPI-Host': 'flight-radar1.p.rapidapi.com'
     }})
-      .then((response) => response.json())
       .then((response) => {
-        // console.log(response)
-        const data = response.result.response.data[0]
-
-        //departure vars
-        let name = data.airport.origin.name
-        let lat = data.airport.origin.position.latitude
-        let lon = data.airport.origin.position.longitude
-        let time = data.time.scheduled.departure
-        setDeparture({time: time, name: name, lat: lat, lon: lon})
-
-        //arrival vars
-        name = data.airport.destination.name
-        lat = data.airport.destination.position.latitude
-        lon = data.airport.destination.position.longitude
-        time = data.time.scheduled.arrival
-        setArrival({time: time, name: name, lat: lat, lon: lon})
+        console.log("Response received")
+        return response.json()
       })
+      .then((response) => {
+        // filter for upcoming flights
+        console.log("Filtering by time")
+        const data = response.result.response.data.reverse().find(findTimeFlight)
+        console.log(data)
+
+        // departure vars
+        setDeparture({
+          time: data.time.scheduled.departure * 1000,
+          name: data.airport.origin.name,
+          lat: data.airport.origin.position.latitude,
+          lon: data.airport.origin.position.longitude
+        })
+
+        // arrival vars
+        setArrival({
+          time: data.time.scheduled.arrival * 1000,
+          name: data.airport.destination.name,
+          lat: data.airport.destination.position.latitude,
+          lon: data.airport.destination.position.longitude
+        })
+        console.log("Arrival and Departure set")
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+
+  function findTimeFlight(period) {
+    // The multiplication compensates for UNIX time things
+    const dept = period.time.scheduled.departure * 1000
+    const now = Date.now()
+    console.log("Scheduled > Now?")
+    console.log(dept, dept > now)
+    return (dept > now)
   }
 
   function SearchBar({onSubmit}) {
@@ -73,55 +94,83 @@ export default function App() {
 
 
 function WeatherCard({city}) {
+  //Update these values to display a fake flight or something??  Or implement logic in render
 
   const [temp, setTemp] = useState(null)
   const [humid, setHumid] = useState(null)
   const [icon, setIcon] = useState(null)
   const [wind, setWind] = useState(null)
   const [desc, setDesc] = useState(null)
+  const [timeString, setTimeString] = useState(null)
 
-  // Call Weather API (this can be consolidated)
+  // Call Weather API
   function weatherCall(lat, lon) {
     // We'll need this because the national weather service has a two request system
     const coordsURL = `https://api.weather.gov/points/${lat},${lon}`
     fetch(coordsURL, {method: "GET"})
       .then((response) => response.json())
       .then((response) => {
-        callGrid(response.properties.forecastHourly)
+        console.log(response)
+        callGrid(response.properties.forecastHourly, city.time)
+    }).catch((error) => {
+      console.log(error)
     })
+
   }
 
-
-  function callGrid(url) {
+  // This fires a million times for some reason???
+  function callGrid(url, time) {
     fetch(url, {method: "GET"})
       .then((response) => response.json())
       .then((response) => {
-        // need logic to decide which period to use
-        const today = response.properties.periods[0]
-        console.log(today)
-        setTemp(today.temperature)
-        setDesc(today.shortForecast)
-        setHumid(today.relativeHumidity.value)
-        setIcon(today.icon)
-        setWind(today.windSpeed)
+        // Checks which time period to use
+        console.log("Checking weather")
+        const period = findTimeWeather(response, time)
+        console.log(period)
+
+        // Sets based on that period
+
+        setTemp(period.temperature)
+        setDesc(period.shortForecast)
+        setHumid(period.relativeHumidity.value)
+        setIcon(period.icon)
+        setWind(period.windSpeed)
+    }).catch((error) => {
+      console.log(error)
     });
   }
 
-  if (city.name) weatherCall(city.lat, city.lon);
+  function findTimeWeather(response, time) {
+    let match = {}
+    // could use .find in here, rather than all this nonsense
+    response.properties.periods.forEach((period) => {
+      const start = Date.parse(period.startTime)
+      const end = Date.parse(period.endTime)
+      if (start < time && end > time) {
+        console.log("Period matched")
+        match = period
+      }
+    });
+    return match
+  }
 
-  // Display info (with various intermediate steps tbd)
-  return (
-    <div className="weather-card">
-      <div className="title-row">
-        <h2>{city.name}</h2>
+  if (city.name) {
+    weatherCall(city.lat, city.lon);
+    // Display info
+    return (
+      <div className="weather-card">
+        <div className="title-row">
+          <h2>{city.name}</h2>
+          <p>{timeString}</p>
+        </div>
+        <div className="content-row">
+          <img src={icon} alt="weather icon" className="weather-icon"/>
+          <h3>{temp}°F</h3>
+          <p>{desc}</p>
+          <p>Wind: {wind}</p>
+          <p>Humidity: {humid}%</p>
+        </div>
       </div>
-      <div className="content-row">
-        <img src={icon} alt="weather icon" className="weather-icon"/>
-        <h3>{temp}°F</h3>
-        <p>{desc}</p>
-        <p>Wind: {wind}</p>
-        <p>Humidity: {humid}%</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
